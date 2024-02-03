e92351b797f079f3036c7a2cbaaa2946e5daafdc
Mohsen <mohsn.mirzaei@gmail.com>
2024-02-03 16:06:43 +0330

    Fixed API base URL

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClinet = axios.create({
-  baseURL: "https://usab.saeidtavazani.ir/app/usab/api/v1",
+  baseURL: "https://usub.mohsenmirzaei.dev/app/usub/api/v1",
 });
 
 export default apiClinet;
